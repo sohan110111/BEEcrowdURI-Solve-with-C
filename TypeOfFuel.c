cc11844496f1d5fa556ea8bbf7f02312106d427c
sohan110111 <sohan110111@gmail.com>
2022-10-11 15:30:00 +0600

    1134 type of fuel

---
+++ TypeOfFuel.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int n, a=0, g=0, d=0;
+    while(scanf("%d", &n)) {
+        if (n == 4)
+        {
+            break;
+        }
+        
+        if(n == 1) {
+            a++;
+        }else if(n == 2) {
+            g++;
+        }else if(n == 3) {
+            d++;
+        }
+    }
+    
+    printf("MUITO OBRIGADO\nAlcool: %d\nGasolina: %d\nDiesel: %d\n", a, g, d);
+    return 0;
+}
