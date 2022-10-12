cdc5a9d0c5d5a2896c4bc99674240eb40d0a3f3c
sohan110111 <sohan110111@gmail.com>
2022-10-12 23:42:47 +0600

    1146 - Growing Sequences

---
+++ GrowingSequence.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int x = 1;
+    while (x != 0) {
+        scanf("%d", &x);
+        for(int i = 1; i<=x; i++) {
+           if(i == x) {
+                printf("%d\n", i);
+           } else {
+                printf("%d ", i);
+           }
+        }
+    }
+ 
+    return 0;
+}
