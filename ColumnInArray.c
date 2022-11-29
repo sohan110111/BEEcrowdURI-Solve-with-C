e435db41fb34c8d6ac552c917b3291e784c65274
sohan110111 <sohan110111@gmail.com>
2022-11-29 23:54:01 +0600

    1182 - Column in Array

---
+++ ColumnInArray.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int row=12,  col=12, l;
+    float m[row][col], sum = 0.0, avg = 0.0;
+    char t;
+    scanf("%d\n%c", &l, &t);
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++) {
+            scanf("%f", &m[i][j]);
+        }
+    }
+    for(int j = 0; j < row; j++) {
+        sum += m[j][l];
+    }
+    if( t == 'S') {
+        printf("%.1f\n", sum);
+    } else {
+        printf("%.1f\n", sum/col);
+    }
+ 
+    return 0;
+}
