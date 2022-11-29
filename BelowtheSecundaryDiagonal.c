cd2ead1d5359f09c811e190ef673960cbd1d9b8c
sohan110111 <sohan110111@gmail.com>
2022-11-30 00:28:23 +0600

    1186 - Below the Secundary Diagonal

---
+++ BelowtheSecundaryDiagonal.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int row=12,  col=12, l;
+    float m[row][col], sum = 0.0, avg = 0.0, forAvg = 0.0;
+    char t;
+    scanf("%c\n", &t);
+    for(int i = 0; i < row; i++){
+        for(int j = 11; j >= 0; j--) {
+            scanf("%f", &m[i][j]);
+            if(i > j) {
+                sum += m[i][j];
+                forAvg++;
+            }
+        }
+    }
+    if( t == 'S') {
+        printf("%.1f\n", sum);
+    } else {
+        printf("%.1f\n", sum/forAvg);
+    }
+ 
+    return 0;
+}
